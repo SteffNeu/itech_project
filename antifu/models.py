040ce0d165b612743152254314ae89f37b1d5b1d
Kleanthis Symeonides <2339297s@student.gla.ac.uk>
2018-02-23 13:47:06 +0000

    added forms.py,registration templates changed models.py

--- antifu/models.py
+++ antifu/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Category(models.Model):
@@ -15,3 +17,69 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+# Create your models here.
+# class Category(models.Model):
+#     name = models.CharField(max_length=128, unique=True)
+#
+#     def save(self, *args, **kwargs):
+#         super(Category, self).save(*args, **kwargs)
+#
+#     class Meta:
+#         verbose_name_plural = 'Categories'
+#
+#     def __str__(self):
+#         return self.name
+
+
+class Comment(models.Model):
+    comment = models.TextField(max_length=1024)
+    loveliness = models.IntegerField()
+    burnfactor = models.IntegerField()
+    logicRating = models.IntegerField()
+    accuracyRating = models.IntegerField()
+
+    def __str__(self):
+        return self.comment
+
+
+class Post(models.Model):
+    category = models.ManyToManyField(Category)
+    comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    context = models.TextField(max_length=512)
+    tags = models.CharField(max_length=128)
+    grammarFail = models.IntegerField()
+    logicFail = models.IntegerField()
+    toxicity = models.IntegerField()
+    harmful = models.IntegerField()
+    report = models.IntegerField()
+    views = models.IntegerField()
+    picturePost = models.ImageField()
+
+    def __str__(self):
+        return self.picturePost
+
+
+class User(models.Model):
+    comments = models.ForeignKey(Comment)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=128, unique=True)
+    username = models.CharField(max_length=128, unique=True)
+    #profileRef = models.ImageField()
+
+    def __str__(self):
+        return self.username
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+
+
--- itech_project/urls.py
+++ itech_project/urls.py
@@ -20,6 +20,14 @@ from django.conf.urls import include
 from antifu import views
 from django.conf import settings
 from django.conf.urls.static import static
+from registration.backends.simple.views import RegistrationView
+from antifu.forms import UserForm
+
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/antifu/'
+
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
@@ -29,5 +37,8 @@ urlpatterns = [
 	# the antifu application
 	url(r'^admin/', admin.site.urls),
     # urls for adding the account settings of django:
+    url(r'^accounts/register/$',
+        RegistrationView.as_view(form_class=UserForm),
+        name='registration_register', ),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
