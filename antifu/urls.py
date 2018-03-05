772c938b28276e18d585e34bcd9c0a15348f8cf2
Tom Decke <2354160d@student.gla.ac.uk>
2018-03-05 12:19:22 +0000

    added comment and post code/css not yet integrated because I'm hating this and are going for lunch now

--- antifu/urls.py
+++ antifu/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
 		views.show_category, name='show_category'),
 	url(r'^profile/(?P<username>[\w\-]+)/$', views.profile, name='profile'),
+	
 	# url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
 	# url(r'^register/$', views.register, name='register'),
 	# url(r'^login/$', views.user_login, name='login'),
