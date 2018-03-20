19933d2bd191cb3d8559230dd4dd6ea96339f1cd
Kleanthis Symeonides <2339297s@student.gla.ac.uk>
2018-03-20 16:24:30 +0000

    Added confirmation window for deleting account

---
+++ static/js/MySettingsTab.js
@@ -0,0 +1,37 @@
+//found from https://www.w3schools.com/howto/howto_css_modals.asp
+// Get the modal
+function clickityClick() {
+    var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+//////Content inside modal window///////
+
+    var noBtn = document.getElementById('noBtn');
+//If the user pressed no
+    noBtn.onclick = function () {
+        modal.style.display = 'none';
+    }
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+//////////////////////////////////////////
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
