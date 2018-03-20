d29076b4d8655e046cd5535749aa64daf6ecf5a7
Kleanthis Symeonides <2339297s@student.gla.ac.uk>
2018-03-20 18:14:39 +0000

    Fixed modal in MySettingsTab

--- static/js/MySettingsTab.js
+++ static/js/MySettingsTab.js
@@ -1,32 +1,31 @@
 //found from https://www.w3schools.com/howto/howto_css_modals.asp
 // Get the modal
-function clickityClick() {
+$(document).ready(function() {
     var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
     var btn = document.getElementById("myBtn");
-
-//////Content inside modal window///////
-
-    var noBtn = document.getElementById('noBtn');
-//If the user pressed no
-    noBtn.onclick = function () {
-        modal.style.display = 'none';
-    }
-
 // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
+
+    var noBtn = document.getElementById('noBtn');
+
+    noBtn.onclick = function () {
+        alert('Pressed no');
+        modal.style.display = 'none';
+    };
+
 
 // When the user clicks the button, open the modal
     btn.onclick = function () {
         modal.style.display = "block";
-    }
+    };
 //////////////////////////////////////////
 
 // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
         modal.style.display = "none";
-    }
+    };
 
 // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
@@ -35,3 +34,4 @@ function clickityClick() {
         }
     }
 }
+);
