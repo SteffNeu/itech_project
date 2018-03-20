8d9d62746c94a3b73632a36c08035c58f614c28d
Tom Decke <2354160d@student.gla.ac.uk>
2018-03-20 11:37:59 +0000

    test post model

--- antifu/tests.py
+++ antifu/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from antifu.models import Post,Comment,Category
+from datetime import date
 
 # Create your tests here.
 class PostMethodTests(TestCase):
@@ -27,3 +28,36 @@ class PostMethodTests(TestCase):
         post = Post(category=cat,toxicity=0)
         post.save()
         self.assertEqual((post.toxicity >= 0), True)
+
+    def test_ensure_harmful_is_positive(self):
+        # create test category for reference
+        cat = Category(name="test")
+        cat.save()
+        post = Post(category=cat,harmful=0)
+        post.save()
+        self.assertEqual((post.harmful >= 0), True)
+
+    def test_ensure_correct_date(self):
+        # create test category for reference
+        cat = Category(name="test")
+        cat.save()
+        post = Post(category=cat)
+        post.save()
+        #get current date
+        today = date.today()
+        self.assertEqual((post.date == today), True)
+
+    def test_ensure_id_increment(self):
+        cat = Category(name="test")
+        cat.save()
+        first_post = Post(category=cat)
+        first_post.save()
+        second_post = Post(category=cat)
+        second_post.save()
+
+        self.assertIs(1,second_post.id-first_post.id)
+
+    def test_ensure_category(self):
+        i = 3
+        #post = Post()
+        #post.save()
