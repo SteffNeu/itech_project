8bf82515f81d60539ea32b81c3d1ca8c2b2ac059
Tom Decke <2354160d@student.gla.ac.uk>
2018-03-20 10:51:33 +0000

    test classes

--- antifu/tests.py
+++ antifu/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from antifu.models import Post,Comment,Category
 
 # Create your tests here.
+class PostMethodTests(TestCase):
+
+    def test_ensure_grammarFail_is_positive(self):
+        # create test category for reference
+        cat = Category(name="test")
+        cat.save()
+        post = Post(category=cat,grammarFail=0)
+        post.save()
+        self.assertEqual((post.grammarFail >= 0), True)
+
+    def test_ensure_logicFail_is_positive(self):
+        # create test category for reference
+        cat = Category(name="test")
+        cat.save()
+        post = Post(category=cat,logicFail=0)
+        post.save()
+        self.assertEqual((post.logicFail >= 0), True)
+
+    def test_ensure_toxicity_is_positive(self):
+        # create test category for reference
+        cat = Category(name="test")
+        cat.save()
+        post = Post(category=cat,toxicity=0)
+        post.save()
+        self.assertEqual((post.toxicity >= 0), True)
