fa5c1790126be9f0f8cb5877232ce63b51c597a8
Tom Decke <2354160d@student.gla.ac.uk>
2018-03-21 14:53:56 +0000

    appending comments biches (and media_url works)

--- antifu/views.py
+++ antifu/views.py
@@ -147,7 +147,8 @@ def submit_comment(request):
         post = Post.objects.get(id=post_id)
         new_comment = Comment.objects.create(post=post,user=userProfile, comment=comment)
         new_comment.save()
-    return HttpResponse(new_comment)
+
+        return render(request, 'antifu/comment.html', {'comment': new_comment})
 
 
 @login_required
--- static/js/contentFunctionality.js
+++ static/js/contentFunctionality.js
@@ -1,19 +1,19 @@
 
 $(document).ready(function(){
-    var currentEdit = "no_comment"
-    var user_stauts = document.getElementById("user_authentication").innerHTML
-    var user_authenticated = (user_stauts == 'true')
+    var currentEdit = "no_comment";
+    var user_stauts = document.getElementById("user_authentication").innerHTML;
+    var user_authenticated = (user_stauts == 'true');
 
 
     $('.report').click( function(source){
 
 
-        var input = source.target.id
-        var split = input.split("_")
-        var id = split[0]
-        var flag = split[1]
+        var input = source.target.id;
+        var split = input.split("_");
+        var id = split[0];
+        var flag = split[1];
 
-        var wasPressed =  document.getElementById(input).disabled
+        var wasPressed =  document.getElementById(input).disabled;
         if(!wasPressed){
                 var r = confirm("Do you want to report this content?");
                 if (r == true) {
@@ -25,7 +25,7 @@ $(document).ready(function(){
             var data = {
                 id:id,
                 flag: flag,
-            }
+            };
 
             $.ajax({
                 type: "POST",
@@ -48,43 +48,43 @@ $(document).ready(function(){
     $('.edit').click( function(source){
 
         if (currentEdit == "no_comment"){
-            var input = source.target.id
-            var split = input.split("_")
-            var comment_id = split[0]
-            var reference = "#"+comment_id+"_comment"
+            var input = source.target.id;
+            var split = input.split("_");
+            var comment_id = split[0];
+            var reference = "#"+comment_id+"_comment";
 
-            currentEdit = reference
+            currentEdit = reference;
 
-            var data = $(reference)
-            data.attr("contentEditable","true")
+            var data = $(reference);
+            data.attr("contentEditable","true");
             data.css({"border-color": "#C1E0FF",
                  "border-width":"2px",
                  "border-style":"solid"});
 
-            var buttonID = comment_id+"_submit_change"
+            var buttonID = comment_id+"_submit_change";
             document.getElementById(buttonID).style.visibility = "visible"
         }
     });
 
     $('.submit_change').click(function (source) {
-        var input = source.target.id
-        var split = input.split("_")
-        var comment_id = split[0]
-        var buttonID = comment_id+"_submit_change"
-        var data = $(currentEdit)
+        var input = source.target.id;
+        var split = input.split("_");
+        var comment_id = split[0];
+        var buttonID = comment_id+"_submit_change";
+        var data = $(currentEdit);
 
-        var url = source.target.getAttribute('data-url')
+        var url = source.target.getAttribute('data-url');
 
-        data.attr("contentEditable","false")
+        data.attr("contentEditable","false");
         data.css({"border-style":"none"});
-        currentEdit = "no_comment"
+        currentEdit = "no_comment";
         document.getElementById(buttonID).style.visibility = "hidden"
-        var n_comment= data["0"].innerText
+        var n_comment= data["0"].innerText;
 
         var info = {
                 comment_id:comment_id,
                 comment_content:n_comment,
-        }
+        };
 
         $.ajax({
             type: "POST",
@@ -96,7 +96,7 @@ $(document).ready(function(){
                 alert("something went wrong with updating the comment")
             }
         });
-    })
+    });
 
 
 
@@ -106,12 +106,12 @@ $(document).ready(function(){
             return
         }
             <!--get toggle status-->
-        var toggled = source.target.getAttribute("aria-pressed")
+        var toggled = source.target.getAttribute("aria-pressed");
         var id = source.target.id;
 
-        var split = id.split("_")
-        var post_id = split[0]
-        var feat = split[1]
+        var split = id.split("_");
+        var post_id = split[0];
+        var feat = split[1];
 
         var count_id = id+"_count";
         var count = document.getElementById(count_id).innerHTML;
@@ -129,7 +129,7 @@ $(document).ready(function(){
             post_id:post_id,
             feat:feat,
             value: count,
-        }
+        };
 
             $.ajax({
                     type: "POST",
@@ -138,11 +138,11 @@ $(document).ready(function(){
                     success: function(responseData) {
                     },
                     error: function(){
-                        alert("something went wrong with updating post ratings")
+                        alert("something went wrong with updating post ratings");
                     }
                 });
 
-    })
+    });
 
     $('.c_icon').click( function(source){
         if(!user_authenticated){
@@ -150,12 +150,12 @@ $(document).ready(function(){
         }
 
         <!--get toggle status-->
-        var toggled = source.target.getAttribute("aria-pressed")
+        var toggled = source.target.getAttribute("aria-pressed");
         var id = source.target.id;
 
-        var split = id.split("_")
-        var comment_id = split[0]
-        var feat = split[1]
+        var split = id.split("_");
+        var comment_id = split[0];
+        var feat = split[1];
 
         var count_id = id+"_count";
         var count = document.getElementById(count_id).innerHTML;
@@ -173,7 +173,7 @@ $(document).ready(function(){
             comment_id:comment_id,
             feat:feat,
             value: count,
-        }
+        };
 
         $.ajax({
                     type: "POST",
@@ -187,30 +187,32 @@ $(document).ready(function(){
                 });
 
 
-    })
+    });
 
     $(".commentForm").submit(function(event) {
-        event.preventDefault()
-        var source = event.target
-        var post_id = source.getAttribute('data-postID')
-        var user = source.getAttribute('data-user')
-        var url = source.getAttribute('data-url')
-        var comment = document.getElementById(post_id+'_content').value
+        event.preventDefault();
+        var source = event.target;
+        var post_id = source.getAttribute('data-postID');
+        var user = source.getAttribute('data-user');
+        var url = source.getAttribute('data-url');
+        var comment = document.getElementById(post_id+'_content').value;
 
         //after submitting the statement empty the textfield
-        document.getElementById(post_id+'_content').value = ""
+        document.getElementById(post_id+'_content').value = "";
 
         var data = {
             user:user,
             post_id:post_id,
             comment:comment,
-        }
+        };
 
         $.ajax({
                     type: "POST",
                     url: url,
                     data: data,
                     success: function(responseData) {
+   						var commentContainer = $('ul.comments_for_post[data-postid=' + post_id + ']');
+						commentContainer.append(responseData);
                     },
                     error: function(){
                         alert("something went wrong with submitting the comment")
